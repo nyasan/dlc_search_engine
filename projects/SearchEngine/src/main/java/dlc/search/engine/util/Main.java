71140ff3b489b7b5524d04982cfeeaf0bff1f0b6
Ariel Franco <agfranco19@gmail.com>
2015-04-16 00:51:39 -0300

    Agrego algunos txt y el metodo que extrae las palabras de un archivo

---
+++ projects/SearchEngine/src/main/java/dlc/search/engine/util/Main.java
@@ -0,0 +1,13 @@
+package dlc.search.engine.util;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		String doc = "d1.txt";
+		System.out.println(ProcessorUtils.process(doc));
+
+	}
+
+}
---
+++ projects/SearchEngine/src/main/java/dlc/search/engine/util/ProcessorUtils.java
@@ -0,0 +1,64 @@
+package dlc.search.engine.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+public abstract class ProcessorUtils {
+
+	/**
+	 * Retorna un mapa con las palabras contenidas en un documento junto a la frecuencia de cada palabra
+	 * @param file el archivo a procesar
+	 * @return
+	 */
+	public static Map<String, Integer> process(File file) {
+		// String doc = "41575-8.txt";
+		Map<String, Integer> mapa = new HashMap<>();
+		
+		try {
+			try (FileInputStream fis = new FileInputStream(file)) {
+				
+				try (Scanner scan = new Scanner(fis).useDelimiter(Pattern
+						.compile("[^áéñóíúüÁÉÑÓÍÚÜa-zA-Z]"))) {
+
+					while (scan.hasNext()) {
+						String aux = scan.next().toLowerCase();
+						if (aux.length() > 1) {
+							if (!mapa.containsKey(aux)) {
+								mapa.put(aux, 1);
+							} else {
+								mapa.put(aux, mapa.get(aux) + 1);
+							}
+						}
+					}
+				}
+			}
+		} catch (IOException ex) {
+			Logger.getLogger(ProcessorUtils.class.getName()).log(Level.SEVERE,
+					null, ex);
+		}
+		
+		return mapa;
+	}
+	
+	// DESCOMENTAR ESTA IMPLEMENTACION
+	/*
+	public static Map<String, Integer> process(String path) {
+		return process(new File(path));
+	}
+	*/
+	
+	// DEBUG
+	public static Map<String, Integer> process(String doc) {
+		File f = new File("src/main/webapp/resources/docs/" + doc);
+		return process(f);
+	}
+	
+}
